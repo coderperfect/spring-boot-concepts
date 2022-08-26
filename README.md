2dbf5496e4a8e65af7d7d310cb19f08b91590723
coderperfect <mrityunjoynath38@gmail.com>
2022-08-26 20:58:18 +0530

    Profiles

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot Concepts
+
+## Run with a profile
+* Local: mvn spring-boot:run -Dspring-boot.run.profiles=prod
+* Deployment: java -Dspring-boot.run.profiles=prod -jar app.jar
--- src/main/java/com/mrityunjoy/springbootconcepts/SpringBootConceptsApplication.java
+++ src/main/java/com/mrityunjoy/springbootconcepts/SpringBootConceptsApplication.java
@@ -1,6 +1,7 @@
 package com.mrityunjoy.springbootconcepts;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 
 import com.mrityunjoy.springbootconcepts.model.Student;
 
@@ -18,14 +20,36 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringBootConceptsApplication {
 
 	private static AnnotationConfigApplicationContext annotationConfigApplicationContext;
+	private static Environment environment;
+	
+	private static String deploymentRegion;
+	private static String commonName;
 
 	@Autowired
-	public SpringBootConceptsApplication(AnnotationConfigApplicationContext annotationConfigApplicationContext) {
+	public SpringBootConceptsApplication(AnnotationConfigApplicationContext annotationConfigApplicationContext,
+			Environment environment) {
 		SpringBootConceptsApplication.annotationConfigApplicationContext = annotationConfigApplicationContext;
+		SpringBootConceptsApplication.environment = environment;
+	}
+	
+	@Value("${common.name:#{null}}")
+	public void setCommonName(String commonName) {
+		SpringBootConceptsApplication.commonName = commonName;
+	}
+	
+	@Value("${deployment.region:#{null}}")
+	public void setDeploymentRegion(String deploymentRegion) {
+		SpringBootConceptsApplication.deploymentRegion = deploymentRegion;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootConceptsApplication.class, args);
+		
+		log.info(String.format("App author: %s", environment.getProperty("common.author")));
+		log.info(String.format("App name: %s", commonName));
+		
+		log.info(String.format("App started in: %s environment", environment.getProperty("deployment.environment")));
+		log.info(String.format("App started in: %s region", deploymentRegion));
 		
 		Student beluga = annotationConfigApplicationContext.getBean("beluga", Student.class);
 		
